88a15a48a13144ed7501b41f22b2de204510848b
Shawn Halayka <sjhalayka@gmail.com>
2024-07-04 12:35:44 -0600

    Add files via upload

--- ortho.vs.glsl
+++ ortho.vs.glsl
@@ -4,8 +4,12 @@ layout(location = 1) in vec2 texcoord;
 
 out vec2 ftexcoord;
 
+uniform int viewport_width;
+uniform int viewport_height;
+
 void main()
 {
-    gl_Position = position;
+	gl_Position = position;
+
     ftexcoord = texcoord;
 }
