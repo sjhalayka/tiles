aad37b3661182eecd2e32acc7b949d5ca6797ea1
Shawn Halayka <sjhalayka@gmail.com>
2024-07-07 15:25:12 -0600

    Delete circles.vs.glsl

--- circles.vs.glsl
+++
@@ -1,9 +0,0 @@
-#version 430
-layout (location = 0) in vec4 position;
-
-//uniform mat4  u_modelviewprojection_matrix;
-
-void main()
-{
-    gl_Position = position;// u_modelviewprojection_matrix* vec4(position, 1.0);
-}
