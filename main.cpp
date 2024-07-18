5735a98234960bc18d7a6be0ec5469e4e7caf0af
Shawn Halayka <sjhalayka@gmail.com>
2024-07-18 11:13:54 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1128,7 +1128,7 @@ int main(int, char**)
 
 						glm::vec3 quad_centre = (q.vertices[0] + q.vertices[1] + q.vertices[2] + q.vertices[3]) * 0.25f;
 
-						pair<size_t, size_t> centre_index = make_pair(1 + ceil(-zoomed_image_anchor.x / (block_size)+quad_centre.x / (block_size * zoom_factor)), -1 + ceil(-zoomed_image_anchor.y / (block_size)+(quad_centre.y) / (block_size * zoom_factor)));
+						pair<size_t, size_t> centre_index = make_pair( (-zoomed_image_anchor.x / (block_size)+quad_centre.x / (block_size * zoom_factor)), (-zoomed_image_anchor.y / (block_size)+(quad_centre.y) / (block_size * zoom_factor)));
 
 						paste_to_draw.insert(centre_index);
 					}
