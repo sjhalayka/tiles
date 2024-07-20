a12061c59a77f9d087171f0d1b549c6d7ada713f
Shawn Halayka <sjhalayka@gmail.com>
2024-07-20 09:21:11 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1218,25 +1218,21 @@ int main(int, char**)
 
 				if (selected_indices.size() == 0 || selected_indices.end() != selected_indices.find(pair_index))
 				{
-					//pair<size_t, size_t> mouse_plus(copy_paste_mouse_position_index.first + (float)copy_paste_relative_index.first, copy_paste_mouse_position_index.second + (float)copy_paste_relative_index.second);
+					pair<size_t, size_t> mouse_plus(copy_paste_mouse_position_index.first + (float)copy_paste_relative_index.first, copy_paste_mouse_position_index.second + (float)copy_paste_relative_index.second);
 
 					//cout << "copy_paste_mouse_position_index: " << copy_paste_mouse_position_index.first << " " << copy_paste_mouse_position_index.second << endl;
 					//cout << "copypaste relative index: " << copy_paste_relative_index.first << " " << copy_paste_relative_index.second << endl;
 					//cout << "mouse plus: " << mouse_plus.first << " " << mouse_plus.second << endl;
 
-					//size_t copy_index = mouse_plus.first * tiles_per_dimension + mouse_plus.second;
+					size_t copy_index = mouse_plus.first * tiles_per_dimension + mouse_plus.second;
 
 
-					ImVec2 uv_step = ImVec2(float(background_tiles[index].tile_size) / main_tiles_width, float(background_tiles[index].tile_size) / main_tiles_height);
+					ImVec2 uv_step = ImVec2(float(background_tiles[index].tile_size) / (float)main_tiles_width, float(background_tiles[index].tile_size) / (float)main_tiles_height);
 					
-
-
-
-
-					background_tiles[index].uv_min.x = copy_background_tiles[index].uv_min.x + uv_step.x * copy_paste_relative_index.first;// copy_background_tiles[copy_index].uv_min;
-					background_tiles[index].uv_min.y = copy_background_tiles[index].uv_min.y + uv_step.y * copy_paste_relative_index.second;// copy_background_tiles[copy_index].uv_min;
-					background_tiles[index].uv_max.x = copy_background_tiles[index].uv_max.x + uv_step.x * copy_paste_relative_index.first;// copy_background_tiles[copy_index].uv_min;
-					background_tiles[index].uv_max.y = copy_background_tiles[index].uv_max.y + uv_step.y * copy_paste_relative_index.second;// copy_background_tiles[copy_index].uv_min;
+					background_tiles[index].uv_min.x = copy_background_tiles[copy_index].uv_min.x;//  +uv_step.x * (copy_paste_relative_index.first);// copy_background_tiles[copy_index].uv_min;
+					background_tiles[index].uv_min.y = copy_background_tiles[copy_index].uv_min.y;// +uv_step.y * (copy_paste_relative_index.second);// copy_background_tiles[copy_index].uv_min;
+					background_tiles[index].uv_max.x = copy_background_tiles[copy_index].uv_max.x;//  +uv_step.x * (copy_paste_relative_index.first);// copy_background_tiles[copy_index].uv_min;
+					background_tiles[index].uv_max.y = copy_background_tiles[copy_index].uv_max.y;//  +uv_step.y * (copy_paste_relative_index.second);// copy_background_tiles[copy_index].uv_min;
 
 
 //					background_tiles[index].uv_max = copy_background_tiles[copy_index].uv_max;
